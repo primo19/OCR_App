6b6e160a7eb939e2401555c6a66405ee501c64a1
primo19 <aldrindeguzman1998@gmail.com>
2020-01-31 15:41:35 +0800

    Registration Setup

---
+++ client/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "login",
+  components: {}
+};
+</script>
---
+++ client/src/views/Register.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="Enter Email" v-model="email" />
+    <br />
+    <input type="password" name="password" placeholder="Enter Password" v-model="password" />
+    <br />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from "@/services/AuthenticationService";
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    async register() {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
